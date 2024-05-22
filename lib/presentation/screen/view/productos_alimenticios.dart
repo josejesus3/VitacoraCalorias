727ccd67a2fe51e1f39a04143ebfd6498d6b2bea
josejesus3 <gutierresrayas88@gmail.com>
2024-05-22 12:09:08 -0600

    TabBar

--- lib/presentation/screen/view/productos_alimenticios.dart
+++ lib/presentation/screen/view/productos_alimenticios.dart
@@ -3,15 +3,91 @@ import 'package:provider/provider.dart';
 import 'package:vitacora_calorias/domain/dataSources/app_fit_datasources.dart';
 import 'package:vitacora_calorias/provider/lista_alimentos.dart';
 
-class ProductosAliment extends StatelessWidget {
+class ProductosAliment extends StatefulWidget {
   const ProductosAliment({super.key});
 
   @override
+  State<ProductosAliment> createState() => _ProductosAlimentState();
+}
+
+class _ProductosAlimentState extends State<ProductosAliment>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     final frutas = context.watch<AppFitDataSources>().responseFrutas;
     final verduras = context.watch<AppFitDataSources>().responseVerduras;
     final agregar = context.watch<ListaAlimentos>();
     final textStyle = Theme.of(context).textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(
+              icon: Icon(Icons.cloud_outlined),
+            ),
+            Tab(
+              icon: Icon(Icons.beach_access_sharp),
+            ),
+            Tab(
+              icon: Icon(Icons.brightness_5_sharp),
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Center(
+            child: _FrutasVerduras(
+                textStyle: textStyle,
+                frutas: frutas,
+                agregar: agregar,
+                verduras: verduras),
+          ),
+          Center(
+            child: Text("It's rainy here"),
+          ),
+          Center(
+            child: Text("It's sunny here"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _FrutasVerduras extends StatelessWidget {
+  const _FrutasVerduras({
+    super.key,
+    required this.textStyle,
+    required this.frutas,
+    required this.agregar,
+    required this.verduras,
+  });
+
+  final TextTheme textStyle;
+  final List<String> frutas;
+  final ListaAlimentos agregar;
+  final List<String> verduras;
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Column(
@@ -28,8 +104,8 @@ class ProductosAliment extends StatelessWidget {
                 vertical: 10,
               ),
               child: GestureDetector(
-                onTap: (){
-                   agregar.agregarAlimentos(frutas, 20,300 );
+                onTap: () {
+                  agregar.agregarAlimentos(frutas, 20, 300);
                 },
                 child: Container(
                   padding:
